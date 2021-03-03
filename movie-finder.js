ad0401885abcc86dab051dc1775e9494c01c645c
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2021-03-03 16:50:43 +0530

    Background image added, Poster not found GIF added

--- movie-finder.js
+++ movie-finder.js
@@ -5,7 +5,8 @@ function find_movie(){
         showTable(false);
     }
     else{
-    httpRequest = new XMLHttpRequest();
+    	try{
+    		httpRequest = new XMLHttpRequest();
     url = "https://www.omdbapi.com/?t="+movie_name+"&apikey=333e06b5";
     httpRequest.open("GET",url);
     httpRequest.send();
@@ -16,41 +17,52 @@ function find_movie(){
                 movie_data = JSON.parse(this.responseText);
                 movie_poster = movie_data.Poster;
                 response = movie_data.Response;
-                if(response=="True"){
-                    showTable(true);
-                    show_image(movie_poster,400,400,"movie poster");
-                    document.getElementById("movie-title").innerHTML = "<b>Movie Title: </b>"+movie_data.Title;
-                    document.getElementById("title").innerHTML = movie_data.Title;
-                    document.getElementById("year").innerHTML = movie_data.Year;
-                    document.getElementById("release-date").innerHTML = movie_data.Released;
-                    document.getElementById("genre").innerHTML = movie_data.Genre;
-                    document.getElementById("productions").innerHTML = movie_data.Production;
-                    document.getElementById("director").innerHTML = movie_data.Director;
-                    document.getElementById("writer").innerHTML = movie_data.Writer;
-                    document.getElementById("cast").innerHTML = movie_data.Actors;
-                    document.getElementById("story").innerHTML = movie_data.Plot;
-                    document.getElementById("language").innerHTML = movie_data.Language;
-                    document.getElementById("country").innerHTML = movie_data.Country;
-                    document.getElementById("awards").innerHTML = movie_data.Awards;
-                    document.getElementById("imdb-id").innerHTML = movie_data.imdbID;
-                    document.getElementById("rating").innerHTML = movie_data.imdbRating;
-                    document.getElementById("box-office").innerHTML = movie_data.BoxOffice;
+                if(response=="True" && movie_poster!='N/A'){
+                    set_data(movie_data,movie_poster);
+                }
+                else if(response=="True" && movie_poster=='N/A'){
+                    alert("Movie found but Poster is not available");
+                    set_data(movie_data,"http://bit.ly/no-poster-found");
                 }
                 else{
                     alert("Movie not found");
                 }
-            }
+    		}
+    	}
         }
+    	catch(err){
+    		alert("Check your internet connection"+err);
+    	}
     }
 }
 function show_image(src, width, height, alt) {
+
     var img = new Image();
     img.src = src;
     img.width = width;
     img.height = height;
     img.alt = alt;
-
     document.getElementById("poster").src = img.src;
+}
+
+function set_data(movie_data,movie_poster){
+    showTable(true);
+    show_image(movie_poster,400,400,"movie poster");
+    document.getElementById("movie-title").innerHTML = "<b>Movie Title: </b>"+movie_data.Title;
+    document.getElementById("title").innerHTML = movie_data.Title;
+    document.getElementById("year").innerHTML = movie_data.Year;
+    document.getElementById("release-date").innerHTML = movie_data.Released;
+    document.getElementById("genre").innerHTML = movie_data.Genre;
+    document.getElementById("productions").innerHTML = movie_data.Production;
+    document.getElementById("director").innerHTML = movie_data.Director;
+    document.getElementById("writer").innerHTML = movie_data.Writer;
+    document.getElementById("cast").innerHTML = movie_data.Actors;
+    document.getElementById("story").innerHTML = movie_data.Plot;
+    document.getElementById("language").innerHTML = movie_data.Language;
+    document.getElementById("country").innerHTML = movie_data.Country;
+    document.getElementById("awards").innerHTML = movie_data.Awards;
+    document.getElementById("rating").innerHTML = movie_data.imdbRating;
+    document.getElementById("box-office").innerHTML = movie_data.BoxOffice;
 }
 function showTable(show)
 {
